3625c7caffdb59790411b09995caaf21fa114a3f
Mohamad Nur Syazwan <syazwan089@gmail.com>
2023-05-16 01:51:49 +0800

    Initial commit

---
+++ src/main/java/com/syazwan/timetrackersystem/DashboardApplication.java
@@ -0,0 +1,15 @@
+package com.syazwan.timetrackersystem;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class DashboardApplication extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        
+    }
+}
---
+++ src/main/java/com/syazwan/timetrackersystem/DashboardController.java
@@ -0,0 +1,2 @@
+package com.syazwan.timetrackersystem;public class DashboardController {
+}
